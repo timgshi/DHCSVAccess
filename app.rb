3008941f9e71c23e01adc65455e8bafd8dd7b76d
Tim Shi <timgshi@gmail.com>
2012-02-24 21:37:07 -0800

    in the middle of shit

--- app.rb
+++ app.rb
@@ -5,13 +5,57 @@ require 'sinatra'
 require 'haml'
 require 'parse-ruby-client'
 
+enable :sessions
+
+helpers do
+    def validate(username, password)
+
+        return username == "test"
+    end
+
+    def is_logged_in?
+    session["logged_in"] == true
+    end
+  
+    def clear_session
+    session.clear
+    end
+  
+    def the_user_name
+        if is_logged_in? 
+            session["username"] 
+        else
+        "not logged in"
+        end
+    end
+end
+
+
 get '/' do 
     @word = "YOU"
     haml :index
 end
 
-post '/feeling' do
-  @feeling = params["feeling"]
-  haml :feeling
+post '/login' do
+  if(validate(params["username"], params["password"]))
+    puts "yay"
+    session["logged_in"] = true
+    session["username"] = params["username"]
+    # NOTE the right way to do messages like this is to use Rack::Flash
+    # https://github.com/nakajima/rack-flash
+    @message = "You've been logged in.  Welcome back, #{params["username"]}"
+    haml :index
+  else
+    puts "error"
+    # See note above
+    @error_message = "Sorry, those credentials aren't valid."
+    haml :login
+  end
+end
+
+get '/logout' do
+  clear_session
+  @message = "You've been logged out."
+  haml :index
 end
 
